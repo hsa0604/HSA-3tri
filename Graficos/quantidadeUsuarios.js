49268cd2ceaa1a13fe55df6a052239b883eb2776
HSA <110574731+hsa0604@users.noreply.github.com>
2024-10-23 21:07:32 -0300

    Update quantidadeUsuarios.js

--- Graficos/quantidadeUsuarios.js
+++ Graficos/quantidadeUsuarios.js
@@ -1,9 +1,13 @@
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json*/'
+    /*Contante para aguardar a resposta, será feita a requisição dos dados da url acima*/
     const res = await fetch(url)
+    /*onde pegar a resposta e o conteúdo*/
     const = await res.jason()
+    /*separar as chaves (por exemplo facebook) dos valores (quantidade usuários) correspondentes*/
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
+    /*gerando os gráficos*/
     const data = [
         {
             x: nomeDasRedes,
@@ -11,9 +15,5 @@ async function quantidadeUsuarios() {
             type: 'bar'
         }
     ]
+    /*colocar o gráfico dentro de uma div*/
     const grafico = document.creatElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
-}
-quantidadeUsuarios()
